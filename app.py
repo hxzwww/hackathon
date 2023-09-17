2ecdaecb3bf472b6f95aa70bb54948b91717ae1a
hxzwww <vakhlov.av@phystech.edu>
2023-09-18 00:56:25 +0300

    rasdf

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from models.networks import define_G
 from collections import OrderedDict
 import socketio
 import base64
+import torch
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
     '/': './static/'
 })
 
-model_dict = load('models/pretrained/weights.pth')
+model_dict = load('models/weights.pth')
 new_dict = OrderedDict()
 for k, v in model_dict.items():
     new_dict["model." + k] = v
@@ -20,7 +21,7 @@ model = define_G(3, 3, 64, 'unet_256', 'batch', True, 'normal', 0.02, [])
 model.load_state_dict(model_dict)
 model.eval()
 transform = transforms.Compose(
-    [transforms.Grayscale(num_output_channels=1), transforms.Resize((256, 256)), transforms.ToTensor()]
+    [transforms.Resize((256, 256)), transforms.ToTensor()]
 )
 
 
@@ -45,12 +46,18 @@ def encode():
     ext = made_name.split('.')[-1]
     return f'data:image/{ext};base64,{base64_utf8_str}'
 
-
+from skimage.color import rgba2rgb
+from skimage.io import imread
 def transform_img():
-    image = Image.open(given_name)
+    image = imread(given_name)
+    image = rgba2rgb(image)
+    image = transform(image)
+    new_shape = (1,) + image.shape
+    image = image.reshape(new_shape)
+    print(image.shape)
     emoji = model(image)
     transform_to_pil = transforms.ToPILImage()
-    emoji = transform_to_pil(emoji)
+    emoji = transform_to_pil(emoji[0])
     emoji = emoji.resize((400, 400))
     emoji.save(made_name)
     return encode()
@@ -63,4 +70,5 @@ def make(sid, data):
     img = base64.b64decode(img)
     with open(given_name, 'wb') as f:
         f.write(img)
+    print(99999999)
     sio.emit('return', transform_img())
